8941839711391b0ccae20b665aa34f1afefa8575
Hamoruton <127tomoharu@gmail.com>
2020-08-04 14:34:50 +0900

    nakahara commit

---
+++ Assets/TESTSCENE/Nakahara/Scripts/RotateObstacle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObstacle : MonoBehaviour
+{
+    // 回転
+    public bool _axisX;
+    public bool _axisY;
+    public bool _axisZ;
+    
+    private float _speed;
+
+    //============================================================
+    // コンストラクタ
+    //============================================================
+    void Start()
+    {
+        _speed = 0.05f;
+    }
+
+    //============================================================
+    // 更新
+    //============================================================
+    void Update()
+    {
+        if (_axisX)
+        {
+            transform.Rotate(new Vector3(_speed, 0, 0));
+        }
+        else if (_axisY)
+        {
+            transform.Rotate(new Vector3(0, _speed, 0));
+        }
+        else if (_axisZ)
+        {
+            transform.Rotate(new Vector3(0, 0, _speed));
+        }
+    }
+}
