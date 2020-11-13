70638ae6bed0209ef3b720723219ae43608f03b3
haru-shinobu <r.t0desu@gmail.com>
2020-11-13 16:51:42 +0900

    舌カウントUIをGameOver時拡大化。GameOverの理由を分かりやすく。なお、UI変更にて再度書き直し必要。 カメラ挙動　角度をわずかに緩くしました。

--- Assets/TESTSCENE/Tamura/Script/GameOverScript.cs
+++ Assets/TESTSCENE/Tamura/Script/GameOverScript.cs
@@ -7,6 +7,8 @@ public class GameOverScript : MonoBehaviour
 {
     [SerializeField, Header("ゲームオーバー用キャンバス")]
     Canvas overCanvas;
+    [SerializeField, Header("橋カウント用キャンバス")]
+    Canvas countCanvas;
     GameObject ImageSprite;
     GameManager gm;
     bool Flag = false;
@@ -68,6 +70,49 @@ public class GameOverScript : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
         MainCam.SetActive(false);
+        var UIcountImage1 = countCanvas.transform.GetChild(0);
+        var UIcountImage2 = countCanvas.transform.GetChild(1);
+        var UIcountImage3 = countCanvas.transform.GetChild(2);
+        UIcountImage1.SetParent(overCanvas.transform);
+        UIcountImage2.SetParent(UIcountImage1.transform);
+        UIcountImage3.SetParent(UIcountImage1.transform);
+
+        
+        var UICountImageRectTrans = UIcountImage1.GetComponent<RectTransform>();
+        //UIアンカー変更（右上->中央）---------------------
+        var parent = UICountImageRectTrans.parent as RectTransform;
+        if (parent == null) { Debug.LogError("Parent cannot find."); }
+        var diffMin = new Vector2(0.5f,0.5f) - UICountImageRectTrans.anchorMin;
+        var diffMax = new Vector2(0.5f,0.5f) - UICountImageRectTrans.anchorMax;
+        UICountImageRectTrans.anchorMin = new Vector2(0.5f, 0.5f);
+        UICountImageRectTrans.anchorMax = new Vector2(0.5f, 0.5f);
+        var diffleft = parent.rect.width * diffMin.x;
+        var diffright = parent.rect.width * diffMax.x;
+        var diffbottom = parent.rect.height * diffMin.y;
+        var difftop = parent.rect.height * diffMax.y;
+
+        UICountImageRectTrans.sizeDelta += new Vector2(diffleft - diffright, diffbottom - difftop);
+        var pivot = UICountImageRectTrans.pivot;
+        UICountImageRectTrans.anchoredPosition-=new Vector2(
+            (diffleft*(1-pivot.x)) + (diffright*pivot.x),
+        (diffbottom * (1 - pivot.y)) + (difftop * pivot.y));
+        //-------------------------------------------------
+        
+        //UI位置変化・大きさ変更
+        var CountPos = UICountImageRectTrans.position;
+        var CountPosHalf = CountPos / 2;
+        var UIScale = UICountImageRectTrans.localScale;
+        float timer = 0;
+        while (timer < 1)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime * 0.25f;
+            UICountImageRectTrans.position = Vector3.Lerp(CountPos, CountPosHalf, timer);
+            UICountImageRectTrans.localScale = Vector3.Lerp(UIScale, new Vector3(10, 10, 10), timer);
+        }
+        UICountImageRectTrans.position = CountPosHalf;
+        
+
         //マテリアル動作
         var mat = ImageSprite.GetComponent<Image>().material;
         mat.SetFloat("_Progress", 1.0f);
