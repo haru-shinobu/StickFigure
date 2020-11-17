8e99ba18895ce6f0b6244071a57f7f288cef0ec5
haru-shinobu <r.t0desu@gmail.com>
2020-11-17 15:15:15 +0900

    Arrow test

--- Assets/Script/StarterScript.cs
+++ Assets/Script/StarterScript.cs
@@ -18,6 +18,9 @@ public class StarterScript : MonoBehaviour
     GameObject Wind;
     [SerializeField]
     GameObject SideWall_child;
+
+    [SerializeField, Header("Arrow")]
+    GameObject Arrow;
     //==================================================================
     // ゲームスタート時のプレイヤーの開始壁をセット
     //==================================================================
@@ -45,6 +48,9 @@ public class StarterScript : MonoBehaviour
             SoundObj.GetComponent<SoundManager>().BGMState();
         var ex = SideWall_child.GetComponent<SpriteRenderer>().bounds.extents;
         G_data.SideWall_Offset = (ex.x < ex.y) ? ex.x : ex.y;
+        var arrowobj = Instantiate(Arrow, StartBox.transform.position - new Vector3(0, 0, StartBox.GetComponent<MeshRenderer>().bounds.extents.z), Quaternion.identity);
+        arrowobj.transform.position -= Vector3.forward;
+
     }
 
     void Start()
