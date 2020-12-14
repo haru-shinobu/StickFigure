fba85e2279e1301dda0f413eac460c454f3bcdb6
haru-shinobu <r.t0desu@gmail.com>
2020-12-15 03:01:42 +0900

    橋バグ修正（元・橋ベース同士の距離に関係なく生成）

--- Assets/bridgeScript.cs
+++ Assets/bridgeScript.cs
@@ -106,13 +106,19 @@ public class bridgeScript : MonoBehaviour
 
     public void RollDestroy(int value)
     {
-        Rigidbody rb = transform.gameObject.AddComponent<Rigidbody>();
-        rb.isKinematic = false;
-        rb.useGravity = true;
-        rb.AddForce(-Vector3.forward * 2, ForceMode.Impulse);
-        rb.AddTorque(Vector3.right * 20, ForceMode.Impulse);
-        rb.AddTorque(Vector3.forward * 10, ForceMode.Impulse);
-        Destroy(this.gameObject, value);
+        if (this.gameObject)
+        {
+            Rigidbody rb = transform.gameObject.AddComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                rb.AddForce(-Vector3.forward * 2, ForceMode.Impulse);
+                rb.AddTorque(Vector3.right * 20, ForceMode.Impulse);
+                rb.AddTorque(Vector3.forward * 10, ForceMode.Impulse);
+            }
+            Destroy(this.gameObject, value);
+        }
     }
     //消去時呼び出し
     void OnDestroy()
