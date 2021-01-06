f8f5403da1d62e3e147812a9a16d01716301598e
yosiki_simada <yothiki321@gmail.com>
2021-01-06 23:21:58 +0900

    アイテム作った

---
+++ Assets/Script/DeepItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeepItem : MonoBehaviour
+{
+    private GameObject gameManager;
+    private GameManager _Manager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        _Manager = gameManager.GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _Manager.ndCountUp();
+        Destroy(this.gameObject);
+    }
+}
