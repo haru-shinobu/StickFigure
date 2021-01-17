ff500ce1388341add1810e61ae13e78389083ca9
yosiki_simada <yothiki321@gmail.com>
2021-01-17 20:24:46 +0900

    セレクトシーン完成

---
+++ Assets/Script/StageSerect/ChangeMaterial.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    [SerializeField] private Material Nolmal_Material = default;
+    [SerializeField] private Material Serect_Material;
+
+    bool SerectFlag = false;
+
+    RectTransform h;
+
+    private float Normal = 400.0f;
+    private float Serect = 500.0f;
+
+    Material material;
+
+    private float _TexOffsetX = 0;
+
+    [SerializeField] private float _MaxOffset = 7.0f; 
+
+    private void Start()
+    {
+        h = this.GetComponent<RectTransform>();
+        material = this.GetComponent<Image>().material;
+    }
+
+    public void None()
+    {
+        material = Nolmal_Material;
+        h.sizeDelta = new Vector2(Normal, Normal);
+        SerectFlag = false;
+    }
+
+    public void Flash()
+    {
+        material = Serect_Material;
+        h.sizeDelta = new Vector2(Serect, Serect);
+        SerectFlag = true;
+    }
+
+    private void Update()
+    {
+        _TexOffsetX += 0.01f;
+
+        if(SerectFlag)
+        {
+            material.SetTextureOffset("_AddTex", new Vector2(_MaxOffset - (_TexOffsetX % _MaxOffset), 0.5f));
+        }
+    }
+}
